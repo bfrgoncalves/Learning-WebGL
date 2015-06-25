b8286419ac1520d574185d014a0ca07c59831e83
bfrgoncalves <brunofiliperg@gmail.com>
2015-06-25 14:53:20 +0100

    test

---
+++ checkQuadrants.js
@@ -0,0 +1,48 @@
+
+function toRad(angle){
+	return angle*(Math.PI/180);
+}
+
+function checkQuadrants(allAngles){
+
+	var restAngle = 0;
+	var sumAngles = 0;
+	var angleToUse;
+	var coordinates = [];
+	var checked = {};
+
+	firstQuadrant(allAngles, function(){
+		console.log(coordinates);
+	});
+
+
+
+
+
+
+
+	function firstQuadrant(angles, callback){
+		
+		for(i in angles){
+			if (!checked[angles[i]]){
+				if(angles[i] + sumAngles <= 90){
+					sumAngles = sumAngles + angles[i];
+					angleToUse = angles[i];
+					checked[angles[i]] = true;
+					if(angleToUse <= 60) coordinates.push([Math.tan(toRad(angleToUse)), 1]);
+					else coordinates.push([1, Math.tan(toRad(angleToUse))]);
+				}
+				else if(sumAngles < 90) {
+					restAngle = sumAngles + angles[i] - 90;
+					angleToUse = angles[i] - restAngle;
+					sumAngles = sumAngles + angleToUse;
+					if(angleToUse <= 60) coordinates.push([Math.tan(toRad(angleToUse)), 1]);
+					else coordinates.push([1, Math.tan(toRad(angleToUse))]);
+				}
+			}
+		}
+		callback();
+	}
+
+
+}
